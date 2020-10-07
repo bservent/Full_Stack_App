5d0687a3270f500a141c39b7e313f9e84e116fb0
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-10-07 13:41:53 -0700

    schema made

--- model/Hike.js
+++ model/Hike.js
@@ -1,6 +1,10 @@
-/* const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+
 const hikeSchema = new mongoose.Schema ({
     Name: { type: String, required: true},
     Location: { type: String, required: true},
-    Length: 
-}) */
+    LengthInMiles: { type: Number, min:1},
+    Difficulty: { type: String, required: true},
+});
+
+module.exports = mongoose.model('Hike', hikeSchema);
