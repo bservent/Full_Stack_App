d5bcbd682e74dc36d882f3839852f9973dbfc3f9
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-10-07 22:40:02 -0700

    post route and show route

--- controllers/hikeController.js
+++ controllers/hikeController.js
@@ -18,21 +18,29 @@ router.get('/new', (req, res) => {
     res.render('hikes/new');
 });
 
-router.post('/new', (req, res) => {
+router.post('/', (req, res) => {
+    const newHike = {
+        name: req.body.name,
+        location: req.body.location,
+        lengthInMiles: req.body.lengthInMiles,
+        difficulty: req.body.difficulty
+    };
     db.Hike.create(req.body, (err, newHike) => {
         if (err) {
             console.log(err);
         }
-        res.redirect(`/hikes/${newHike._id}`)
+        res.redirect('/hikes/:hikeId')
     });
 });
 
-/* router.get('/:hikeId', (req, res) => {
+router.get('/:hikeId', (req, res) => {
     db.Hike.findById(req.params.hikeId, (err, foundHike) => {
         if (err) {
             console.log(err);
         }
-
+        res.render('hikes/show', {
+            hikes: foundHike,
+        });
     });
-}) */
+}); 
 module.exports = router;
